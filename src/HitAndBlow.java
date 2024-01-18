10a716639ea1ccb4bac7c97ea9521a02af87d0e8
sishikawa-rop2312 <tukuyomi610315@gmail.com>
2024-01-18 15:50:19 +0900

    完成

---
+++ src/HitAndBlow.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class HitAndBlow {
+	private ArrayList<Integer> targetNums;
+	private int numberOfDigits;
+	private int count;
+
+	// コンストラクタ
+	public HitAndBlow(int numberOfDigits) {
+		this.numberOfDigits = numberOfDigits;
+		this.count = 0;
+
+		// ターゲットの数字を生成
+		createTargetNums();
+	}
+	
+	// ターゲットの数字を生成
+	private void createTargetNums() {
+		ArrayList<Integer> targetList = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+		Collections.shuffle(targetList);
+		this.targetNums = new ArrayList<>();
+		for (int i = 0; i < numberOfDigits; i++) {
+			this.targetNums.add(targetList.get(i));
+		}
+
+		// Todo デバッグ用なので削除
+		printArray(this.targetNums);
+	}
+
+	// ナンバーを回答
+	public void answerNums(Scanner scanner) {
+		this.count++;
+		System.out.print(numberOfDigits + "桁の数字を入力してください。 >>");
+		int answerIntNums = scanner.nextInt();
+
+		ArrayList<Integer> answerNums = splitInteger(answerIntNums);
+		int[] checkResult = checkHitAndBlow(answerNums);
+		System.out.println("Hit:" + checkResult[0] + ", Blow:" + checkResult[1]);
+
+		// Hit数がターゲット桁数と同じなら全問正解のためクリア
+		if (numberOfDigits == checkResult[0]) {
+			System.out.println("正解です！！！！！！！！！！\n" + count + "回目でクリアしました。");
+			System.exit(0);
+		}
+	}
+
+	// 指定した桁の数字をヒントで表示
+	public void askHint(Scanner scanner) {
+		System.out.print("何桁目の数字を知りたいですか。 >>");
+		int digit = scanner.nextInt();
+		System.out.printf("%d桁目の数字は%dです\n", digit, targetNums.get(digit - 1));
+	}
+
+	// Hit数とBlow数のチェック
+	private int[] checkHitAndBlow(ArrayList<Integer> answerList) {
+		int hit = 0;
+		int blow = 0;
+
+		for (int i = 0; i < answerList.size(); i++) {
+			if (targetNums.contains(answerList.get(i))) {
+				if (targetNums.get(i).equals(answerList.get(i))) {
+					hit++;
+				} else {
+					blow++;
+				}
+			}
+		}
+
+		int[] hitAndBlow = { hit, blow };
+		return hitAndBlow;
+	}
+
+	// 整数をArrayListの要素に分割
+	private ArrayList<Integer> splitInteger(int number) {
+		ArrayList<Integer> digitList = new ArrayList<>();
+
+		while (number > 0) {
+			int digit = number % 10;
+			digitList.add(0, digit);
+			number /= 10;
+		}
+
+		return digitList;
+	}
+
+	// ターゲットナンバーを表示
+	private void printArray(List<Integer> list) {
+		System.out.printf("デバッグ用：%s\n\n", list);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,36 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-		final int NUMBER_OF_DIGITS = 3; // 数字の桁数
-		int count = 0;	// 回答数
+        System.out.print("何桁の数字で遊びますか？ >>");
+        int numberOfDigits = scanner.nextInt();    // 桁数
+        System.out.println("ゲームスタート\n" + numberOfDigits + "桁の数字を当ててください.");
 
-		// ターゲットの数字を生成
-		ArrayList<Integer> targetList = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-		Collections.shuffle(targetList);
-		ArrayList<Integer> targetNums = new ArrayList<>();
-		for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
-			targetNums.add(targetList.get(i));
-		}
+        HitAndBlow hitAndBlow = new HitAndBlow(numberOfDigits);
 
-		printArray(targetNums);
+        while (true) {
+            System.out.print("1.回答 2.ヒント 3.終了 >>");
+            int select = scanner.nextInt();
 
-		System.out.println("ゲームスタート\n" + NUMBER_OF_DIGITS + "桁の数字を当ててください。");
+            switch (select) {
+                case 1:
+                    hitAndBlow.answerNums(scanner);
+                    break;
+                case 2:
+                    hitAndBlow.askHint(scanner);
+                    break;
+                case 3:
+                    System.out.println("ByeBye");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("整数1～3で入力してください");
+                    break;
+            }
+        }
+    }
 
-		while (true) {
-			System.out.print("1.回答 2.ヒント 3.終了 >>");
-			int select = scanner.nextInt();
-			switch (select) {
-				case 1:
-					count++;
-					System.out.print(NUMBER_OF_DIGITS + "桁の数字を入力してください。");
-					int answerIntNums = scanner.nextInt();
-	
-					ArrayList<Integer> answerNums = splitInteger(answerIntNums);
-					int[] checkResult = checkHitAndBlow(targetNums, answerNums);
-					System.out.println("Hit:" + checkResult[0] + ", Blow:" + checkResult[1]);
-					
-					if (NUMBER_OF_DIGITS == checkResult[0]) {
-						System.out.println("正解です！！！！！！！！！！\n" + count + "回目でクリアしました。");
-						return;
-					}
-					break;
-				case 2:
-					System.out.println("ヒント");
-					break;
-				case 3:
-					System.out.println("ByeBye");
-					scanner.close();
-					return;
-				default:
-					System.out.println("整数1～3で入力してください");
-					break;
-			}
-		}
-	}
-
-	private static int[] checkHitAndBlow(ArrayList<Integer> targetList, ArrayList<Integer> answerList) {
-		int hit = 0;
-		int blow = 0;
-		
-		for (int i = 0; i < answerList.size(); i++) {
-			if (targetList.contains(answerList.get(i))) {
-				if (targetList.get(i) == answerList.get(i)) {
-					hit++;
-				} else {
-					blow++;
-				}
-			}
-		}
-		
-		int[] hitAndBlow = { hit, blow };
-
-		return hitAndBlow;
-	}
-
-	// 整数をArrayListの要素に分割
-	private static ArrayList<Integer> splitInteger(int number) {
-		ArrayList<Integer> digitList = new ArrayList<>();
-
-		while (number > 0) {
-			int digit = number % 10;
-			digitList.add(0, digit);
-			number /= 10;
-		}
-
-		return digitList;
-	}
-
-	private static void printArray(List<Integer> list) {
-		System.out.printf("%s\n\n", list);
-	}
 }
